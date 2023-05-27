f06687fa0d505bbea79b69fcc8e352101ff20a7c
Adarsh Sharma <sharma007adarsh@gmail.com>
2023-05-27 17:10:09 +0530

    handled opening new tab and url

--- scrapeFn/scrape.js
+++ scrapeFn/scrape.js
@@ -1,5 +1,20 @@
-async function main(skill) {
+const puppeteer = require('puppeteer');
+const data = {
+    list : []
+};
 
-}
+async function main(skill) {
+    //open chromium 
+    const browser = await puppeteer.launch({ headless: false });
+    //open new tab
+    const page = await browser.newPage();
+
+    await page.goto(`https://in.indeed.com/jobs?q=${skill}&l=Bengaluru%252C+Karnataka`, {
+        timeout: 0,
+        waitUntil: 'networkidle0'
+    });
+    
+    browser.close();
+};
 
 module.exports = main;
